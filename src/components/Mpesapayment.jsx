dffd765382e12c88b3b8c742aa8cd805016b7b4a
tabby <tabbytash35@gmail.com>
2026-03-26 16:58:06 +0300

    second commit

--- src/components/Mpesapayment.jsx
+++ src/components/Mpesapayment.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { useLocation } from "react-router-dom"
 const Mpesapayment = () => {
 
